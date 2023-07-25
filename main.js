d3068129ee6d72de3488af041cd4867a070b71f0
Heidy Prieto <114593515+heidylaborracha@users.noreply.github.com>
2023-07-24 20:40:35 -0500

    Initial Commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const typed = new Typed('.typed', {
+    strings: [
+    '<i class="palabras"><Libera</i>', 
+    '<i class="palabras">Da libertad</i>', 
+    '<i class="palabras">Alegra</i>', 
+    '<i class="palabras">Da Paz</i>', 
+    '<i class="palabras">Relaja</i>', 
+    '<i class="palabras">Enamora</i>'
+],
+stringsElements: '#cadenas-texto',
+typeSpeed: 75,
+startDelay: 300,
+backSpeed: 75,
+smartBackspace: true,
+shuffle: false,
+backDeLay: 1500,
+loop: true,
+loopCount: false,
+showCursor: true,
+cursorCrar: '|',
+contentType: 'html',
+});
